35d851e6dc0a051b53b0811c700dcc553f3341ff
Alejandro <adgm1988@gmail.com>
2019-08-27 00:37:36 -0500

    perdido y ganado untracked

---
+++ resources/views/pages/perdido.blade.php
@@ -0,0 +1,39 @@
+@extends('layout')
+@section('content')
+
+
+@if ($errors->any())
+<div class="alert alert-danger">
+	<strong>Revisa los datos ingreasados<br><br>
+	<ul>
+		@foreach ($errors->all() as $error)
+		<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+
+<form method="post" action="/perdido/{{ $prospecto->id}}" id="form">
+	@csrf
+
+	<div class="row">
+		<div class="form-group col-12">
+			<label for="empresa">Motivo de rechazo:</label>
+			<input type="text" class="form-control" name="motivo" >
+		</div>
+		<div class="form-group col-12">
+			<label for="orden">Notas:</label>
+			<textarea name="notas" class="md-textarea form-control" rows="3"></textarea>
+		</div>
+		<input type="hidden" name="etapa_anterior_id" value="{{ $prospecto->etapas->id }}">
+	</div>
+	
+	<div class="row">
+		<div class="form-group col-md-6">
+			<button class="btn btn-success" >Guardar</button>
+			<button type="button" class="btn btn-secondary" >Cancelar</button>
+		</div>
+	</div>
+</form>
+
+@endsection
