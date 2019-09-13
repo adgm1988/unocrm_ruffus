d7d5e7dcac7fa357ef05ca7bfc4e75c32982bdf9
Alejandro <adgm1988@gmail.com>
2019-09-12 20:34:04 -0500

    cambio orden de seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(RechazoSeeder::class);
         $this->call(TipoactsSeeder::class);
         $this->call(EtapasSeeder::class);
         $this->call(ProcedenciasSeeder::class);
+        $this->call(RechazoSeeder::class);
         $this->call(ProspectosSeeder::class);
         $this->call(ActividadsSeeder::class);
         $this->call(IndustriesSeeder::class);
