8244268ee42abe0bcceae4eebc075941d86eacd6
Alejandro <adgm1988@gmail.com>
2019-10-24 14:06:13 -0500

    calendar controller con if actividades

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -32,30 +32,33 @@ class CalendarController extends Controller
             $actividades = Actividad::all();
         }
 
-    	foreach($actividades as $actividad){
+        if($actividades){
+            foreach($actividades as $actividad){
 
-    		$hora = str_replace(':','',$actividad->hora);
-    		$duracion =  str_replace(':','',$actividad->duracion);
-    		$horafinal = str_pad($hora+$duracion,4,0,STR_PAD_LEFT);
-    		$color = $actividad->color;
-    		$textocalendario = $actividad->prospecto->empresa.'--'.$actividad->descripcion;
-    		$concatfechahorainicial = $actividad->fecha.'T'. $hora;
-    		$concatfechahorafinal = $actividad->fecha.'T'. $horafinal;
-	    	$events[] = \Calendar::event(
-			    $textocalendario, //event title
-			    false, //full day event?
-			    $concatfechahorainicial, //start time (you can also use Carbon instead of DateTime)
-			    $concatfechahorafinal,
-				$actividad->id,
-				[
-					'color' => $color,
-					//'borderColor' => $actividad->prospecto->semaforo,
-					'url' => 'prospectos/'.$actividad->prospecto->id,
-					'description' => $actividad->descripcion,
-					//'editable' => true
-				] //optionally, you can specify an event ID
-			);
-	    }
+                $hora = str_replace(':','',$actividad->hora);
+                $duracion =  str_replace(':','',$actividad->duracion);
+                $horafinal = str_pad($hora+$duracion,4,0,STR_PAD_LEFT);
+                $color = $actividad->color;
+                $textocalendario = $actividad->prospecto->empresa.'--'.$actividad->descripcion;
+                $concatfechahorainicial = $actividad->fecha.'T'. $hora;
+                $concatfechahorafinal = $actividad->fecha.'T'. $horafinal;
+                $events[] = \Calendar::event(
+                    $textocalendario, //event title
+                    false, //full day event?
+                    $concatfechahorainicial, //start time (you can also use Carbon instead of DateTime)
+                    $concatfechahorafinal,
+                    $actividad->id,
+                    [
+                        'color' => $color,
+                        //'borderColor' => $actividad->prospecto->semaforo,
+                        'url' => 'prospectos/'.$actividad->prospecto->id,
+                        'description' => $actividad->descripcion,
+                        //'editable' => true
+                    ] //optionally, you can specify an event ID
+                );
+            }
+        }
+    	
 
 		$calendar = \Calendar::addEvents($events) //add an array with addEvents
 		    ->setOptions([ //set fullcalendar options
