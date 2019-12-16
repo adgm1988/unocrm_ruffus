bebbeeb635737b89faa877954ea4be0d89027918
Alejandro Gzz <adgm1988@gmail.com>
2019-12-16 17:09:51 -0600

    default en meta seed user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,24 +17,28 @@ class UsersTableSeeder extends Seeder
             'email'=>'admin@uno.uno',
             'password'=>bcrypt('12341234'),
             'admin'=>true,
+            'meta'=>0,
         ]);
         User::create([
         	'name'=>'Consultor Demo',
         	'email'=>'consultor@uno.uno',
         	'password'=>bcrypt('12341234'),
             'consultor'=>true,
+            'meta'=>0,
         ]);
         User::create([
             'name'=>'Director Demo',
             'email'=>'director@uno.uno',
             'password'=>bcrypt('12341234'),
             'director'=>true,
+            'meta'=>0,
         ]);
         User::create([
             'name'=>'Vendedor Demo',
             'email'=>'vendedor@uno.uno',
             'password'=>bcrypt('12341234'),
             'vendedor'=>true,
+            'meta'=>0,
         ]);
 
         //Si quisiera crear 5 usuarios aparte de Juan
